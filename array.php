a4d6b85bfc1650957eb18542dcd786443177874f
ratnikproryz <ptpptp1611@gmail.com>
2023-05-24 09:28:16 +0700

    psr formatted

--- array.php
+++ array.php
@@ -1,23 +1,25 @@
 <?php
-$arr = [15, 3,6, 8, 1, 0  - 2, -5, 10, 5];
-$arr2= array();
-$length = count($arr);
-echo "Use for: ";
-for($i=0; $i <$length; $i++){
-    if($arr[$i] > 5){
-        echo $arr[$i]." ";
-    }
+function biggerThan5($var)
+{
+    return $var > 5;
 }
-foreach( $arr as $item){
-    if($item > 5){
-        array_push($arr2, $item);
+
+$listNumbers = [15, 3, 6, 8, 1, 0  - 2, -5, 10, 5];
+$resultList = array();
+$numberItems = count($listNumbers);
+
+echo "Use for: ";
+for ($i = 0; $i < $numberItems; $i++) {
+    if ($listNumbers[$i] > 5) {
+        echo $listNumbers[$i] . " ";
     }
 }
 
-function bigger5($var){
-    return $var> 5;
+foreach ($listNumbers as $item) {
+    if ($item > 5) {
+        array_push($resultList, $item);
+    }
 }
 echo "\nUse filter: ";
-$result = array_filter($arr2, 'bigger5');
+$result = array_filter($resultList, 'biggerThan5');
 print_r($result);
-
